e2529696ede0c5f58ea7e70b5c34f24eeaa52e1a
Leo Rentmeister <leo.v.rentmeister@gmail.com>
2024-10-13 02:14:34 +0200

    add: fast project navigation

--- pm_completion.sh
+++ pm_completion.sh
@@ -22,4 +22,20 @@ _pm_completions() {
         esac
 }
 
+pmcd() {
+    cd "$(pm pwd "$1")"
+}
+
+_pmcd_completions() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(pm list | awk '{print $1}')
+
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+
+complete -F _pmcd_completions pmcd
 complete -F _pm_completions pm
